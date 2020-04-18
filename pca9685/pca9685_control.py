2619035076a368e99cb7883ef579ac1f62db3da3
h-krbys <@>
2020-04-18 19:02:45 +0900

    edit: pca9685 control program
    Change control input.
    Type desired position -> Move all servo -> Wait (2s)

--- pca9685/pca9685_control.py
+++ pca9685/pca9685_control.py
@@ -10,30 +10,11 @@ pwm = Adafruit_PCA9685.PCA9685(address = 0x40, busnum = 1)
 # Number of servo
 servo_num = 12
 
-# Configure min and max servo pulse lengths
-servo_min    = 150 # min. pulse length
-servo_max    = 600 # max. pulse length
-servo_offset = 50
-
 # Set frequency to 60[Hz]
 pwm.set_pwm_freq(60)
 
 while True:
-    # Move servo on each channel
+    pos = input('Enter servo position (150-600): ')
     for i in range(servo_num):
-        print('Moving servo on channel: ', i)
-        pwm.set_pwm(i, 0, servo_min + servo_offset)
-        time.sleep(1)
-    for i in range(servo_num):
-        print('Moving servo on channel: ', i)
-        pwm.set_pwm(i, 0, servo_max - servo_offset)
-        time.sleep(1)
-    # Move servo on all channel
-    for i in range(servo_num):
-        print('Moving servo on channel: ', i)
-        pwm.set_pwm(i, 0, servo_min + servo_offset)
-    time.sleep(1)
-    for i in range(servo_num):
-        print('Moving servo on channel: ', i)
-        pwm.set_pwm(i, 0, servo_max - servo_offset)
-    time.sleep(1)
+        pwm.set_pwm(i, 0, pos)
+    time.sleep(2)
